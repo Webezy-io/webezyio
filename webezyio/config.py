c32dd5e52f28915b409561281a51ecc439889bdb
Amit Shmulevitch <amitsh1995@gmail.com>
2022-12-22 21:30:36 +0200

    docs

--- webezyio/config.py
+++ webezyio/config.py
@@ -12,10 +12,10 @@ configs=WebezyConfig(
     host="localhost",
     port=50051,
     # Analytic gathering approval
-    analytics=False,
+    analytics=True,
     # First run flag
-    first_run=False,
-    token="macOS-12.6-arm64-arm-64bit:2022-12-22T09:04:45.725155",
+    first_run=True,
+    token="",
     # Supported builtins templates
     webezyio_templates = [
         "@webezyio/Blank",
